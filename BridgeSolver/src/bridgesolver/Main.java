daed30ffe30e26b05b9fb20ee3e3033403b315b6
Daniel Magin <danielmagin@live.com>
2013-09-19 11:39:30 +0200

    Removed unused imports

--- BridgeSolver/src/bridgesolver/Main.java
+++ BridgeSolver/src/bridgesolver/Main.java
@@ -4,16 +4,6 @@
  */
 package bridgesolver;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
 /**
  *
  * @author Martin Hoelzel
