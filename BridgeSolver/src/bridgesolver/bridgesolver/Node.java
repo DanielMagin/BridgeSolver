2ac57b52c505687f8454e7983a1f2c8a0f286179
maginda <maginda@MAGINDA2.emea.hpqcorp.net>
2013-09-23 11:12:25 +0200

    removed warnings

---
+++ BridgeSolver/src/bridgesolver/bridgesolver/Node.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bridgesolver;
+
+/**
+ *
+ * @author Martin Hoelzel
+ */
+public class Node {
+
+        public int x;
+        public int y;
+
+        public int value;
+        private int initialValue;
+        private boolean created = false;
+
+        public Node(int x, int y, int value) {
+                this.x = x;
+                this.y = y;
+                this.value = value;
+        }
+
+        @Override
+        public String toString() {
+                String result = "";
+                result += this.formatInt(x);
+                result += this.formatInt(y);
+                return result;
+        }
+
+        public String formatInt(int i){
+                String result = "";
+                if(i<10) result += "0"+i;
+                else result+= i;
+                return result;
+        }
+               
+        public void created(){
+            this.initialValue = this.value;
+            this.created = true;
+        }
+        
+        public int getInitialValue(){
+            return this.initialValue;
+        }
+        
+        public boolean isCreated(){
+            return this.created;
+        }
+}
